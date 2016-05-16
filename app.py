7ecb74087da097c316026c4c05629a4321169aca
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-16 19:21:40 -0400

    initial app code

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from tinydb import TinyDB, Query
+
+import os
+import os.path as op
+
+# Pre-initialization checks
+# 1. Initialize database.
+
+# Initialize app
+app = Flask(__name__)
+db = TinyDB(db_path)
+
+
+@app.route('/')
+def main():
+    """
+    The main page. Lists a calendar view (4 weeks).
+    """
+    return render_template('index.html')
