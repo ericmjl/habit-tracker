6c0a85ee66ad795d3b453415166ac4044e73c9a0
Eric J. Ma <ericmajinglong@gmail.com>
2016-05-26 09:09:06 -0400

    added dates to index page

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from tinydb import TinyDB, Query
+from datetime import datetime, timedelta
 
 import os
 import os.path as op
@@ -17,7 +18,17 @@ def main():
     """
     The main page. Lists a calendar view (4 weeks).
     """
-    return render_template('index.html')
+
+    today = datetime.today()
+    dates = [today - timedelta(2),
+             today - timedelta(1),
+             today,
+             today + timedelta(1),
+             today + timedelta(2)]
+
+    return render_template('index.html',
+                           dates=dates,
+                           today=today)
 
 if __name__ == '__main__':
     app.run(debug=True)
